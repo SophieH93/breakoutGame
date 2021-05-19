ffbcd00bde90dfd693f0f611d5d682187b4c5d39
Sophie <sophiehickey123@gmail.com>
2021-05-19 11:25:51 +0100

    created ball & bricks

--- phase-breakout/src/GameScene.js
+++ phase-breakout/src/GameScene.js
@@ -16,6 +16,10 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     this.player = this.createPlayer();
+    this.ball = this.createBall();
+    this.blueBricks = this.createBlueBricks();
+    this.yellowBricks = this.createYellowBricks();
+    this.redBricks = this.createRedBricks();
   }
 
   update() {}
@@ -23,5 +27,36 @@ export default class GameScene extends Phaser.Scene {
   createPlayer() {
     let player = this.physics.add.sprite(400, 600, "paddle");
     return player;
+  }
+  createBall() {
+    let ball = this.physics.add.sprite(400, 565, "ball");
+    return ball;
+  }
+  createBlueBricks() {
+    let blueBricks = this.physics.add.group({
+      key: "brick1",
+      repeat: 9,
+      imovable: true,
+      setXY: { x: 80, y: 140, stepX: 70 },
+    });
+    return blueBricks;
+  }
+  createYellowBricks() {
+    let yellowBricks = this.physics.add.group({
+      key: "brick2",
+      repeat: 9,
+      imovable: true,
+      setXY: { x: 80, y: 90, stepX: 70 },
+    });
+    return yellowBricks;
+  }
+  createRedBricks() {
+    let redBricks = this.physics.add.group({
+      key: "brick3",
+      repeat: 9,
+      imovable: true,
+      setXY: { x: 80, y: 40, stepX: 70 },
+    });
+    return redBricks;
   }
 }
